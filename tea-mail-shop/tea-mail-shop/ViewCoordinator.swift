62a1a5b3f9a685ff0b3a552bdbe11dd5d6521513
Natallia Mazur <natashamazur@bk.ru>
2024-06-05 15:15:24 +0300

    Add new project with splash view

---
+++ tea-mail-shop/tea-mail-shop/ViewCoordinator.swift
@@ -0,0 +1,24 @@
+//
+//  ViewCoordinator.swift
+//  tea-mail-shop
+//
+//  Created by Наталья Мазур on 5.06.24.
+//
+
+import SwiftUI
+
+struct ViewCoordinator: View {
+    @State private var isActive = false
+    
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            SplashView(isActive: $isActive)
+        }
+    }
+}
+
+#Preview {
+    ViewCoordinator()
+}
---
+++ tea-mail-shop/tea-mail-shop/tea_mail_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  tea_mail_shopApp.swift
+//  tea-mail-shop
+//
+//  Created by Наталья Мазур on 5.06.24.
+//
+
+import SwiftUI
+
+@main
+struct tea_mail_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ViewCoordinator()
+        }
+    }
+}
